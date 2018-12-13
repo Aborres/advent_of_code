97f6539939b4dc0939fa5bafccbb16bc6b2ebb46
Aborres <jmnaranjotemprano@gmail.com>
2018-12-13 22:31:13 +0000

    Adding puzzle 2 in puzzle 13 and skeleton for puzzle 13

---
+++ include/18/14.h
@@ -0,0 +1,14 @@
+//jnaranjo Advent of code 18
+
+#ifndef ADVENT_OF_CODE_18_14
+#define ADVENT_OF_CODE_18_14 (1)
+
+namespace Y18 {
+  namespace D14 {
+
+    void puzzle();
+
+  }
+}
+
+#endif //ADVENT_OF_CODE_18_14
---
+++ src/18/14.cpp
@@ -0,0 +1,16 @@
+//jnaranjo Advent of code 18
+
+#include <18/14.h>
+
+#include <utils.h>
+
+namespace Y18 {
+  namespace D14 {
+
+    void puzzle() {
+
+    }
+
+  }
+}
+
