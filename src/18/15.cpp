8517e682b3f9f5eb4ac75f7902e0878ec572d948
Aborres <jmnaranjotemprano@gmail.com>
2018-12-17 17:10:38 +0000

    Adding skeletons for day 16 and 17, will continue will puzzles asap (vacation)

--- src/18/15.cpp
+++ src/18/15.cpp
@@ -1,10 +1,11 @@
-//jnaranjo Advent of code 18
+﻿//jnaranjo Advent of code 18
 
 #include <18/15.h>
 
 #include <utils.h>
 
 #include <vector>
+#include <deque>
 #include <algorithm>
 
 namespace Y18 {
@@ -60,30 +61,8 @@ namespace Y18 {
 
       vec2i* pos = &s->pos;
       buff[IT(pos->x, pos->y, w)] = Type::Space;
-
-      if (pos->x != new_pos.x) {
-
-        int32 n_x = new_pos.x - pos->x;
-        n_x /= std::abs(n_x);
-
-        if (buff[IT(pos->x + n_x, pos->y, w)] == Type::Space) {
-          pos->x += n_x; 
-          buff[IT(pos->x, pos->y, w)] = s->type;
-          return;
-        }
-      } 
-      
-      if (pos->y != new_pos.y) {
-
-        int32 n_y = new_pos.y - pos->y;
-        n_y /= std::abs(n_y);
-
-        if (buff[IT(pos->x, pos->y + n_y, w)] == Type::Space) {
-          pos->y += n_y; 
-          buff[IT(pos->x, pos->y, w)] = s->type;
-          return;
-        }
-      }
+      buff[IT(new_pos.x, new_pos.y, w)] = s->type;
+      (*pos) = new_pos;
     }
 
     static bool CanAttack(Type* buff, const uint32 w, Soldier* s, 
@@ -139,6 +118,83 @@ namespace Y18 {
         buff[IT(e_pos.x, e_pos.y, w)] = Type::Space;
         s->enemy = nullptr;
       }
+    }
+
+    /*
+     1  function Dijkstra(Graph, source):
+     2
+     3      create vertex set Q
+     4
+     5      for each vertex v in Graph:             // Initialization
+     6          dist[v] ← INFINITY                  // Unknown distance from source to v
+     7          prev[v] ← UNDEFINED                 // Previous node in optimal path from source
+     8          add v to Q                          // All nodes initially in Q (unvisited nodes)
+     9
+    10      dist[source] ← 0                        // Distance from source to source
+    11      
+    12      while Q is not empty:
+    13          u ← vertex in Q with min dist[u]    // Node with the least distance
+    14                                              // will be selected first
+    15          remove u from Q 
+    16          
+    17          for each neighbor v of u:           // where v is still in Q.
+    18              alt ← dist[u] + length(u, v)
+    19              if alt < dist[v]:               // A shorter path to v has been found
+    20                  dist[v] ← alt 
+    21                  prev[v] ← u 
+    22
+    23      return dist[], prev[]
+    */
+
+    static bool FindPath(Type* buff, uint32 w, uint32 size,
+                         const vec2i& from, const vec2i& to, vec2i* out) {
+
+      bool* visited = new bool[size];
+      memset(visited, 0, sizeof(bool) * size);
+
+      // traverse the cave in distance / reading order
+      std::deque<vec2i> open_list {from};
+      visited[IT(from.x, from.y, w)] = true;
+
+      std::vector<vec2i> closed_list;
+
+      vec2i order[] = {
+        {-1, 0}, {0, -1}, {0, 1}, {1, 0}
+      };
+
+      while (open_list.size() > 0) {
+        vec2i front = open_list[0];
+        open_list.pop_front();
+        closed_list.push_back(front);
+
+        if (std::find(closed_list.begin(), closed_list.end(), to) != closed_list.end()) {
+          *out = closed_list[0];
+          return true;
+        }
+
+        uint32 min_dist = 99999;
+        for (uint32 i = 0; i < 4; ++i) {
+          const vec2i dir = order[i];
+          const vec2i c = front + dir;
+          const uint32 dist = Manhattan(c, to);
+          if (dist < min_dist) {
+            min_dist = dist;
+          }
+        }
+
+        for (uint32 i = 0; i < 4; ++i) {
+          const vec2i dir = order[i];
+          const vec2i c = front + dir;
+          const uint32 it = IT(c.x, c.y, w);
+          const uint32 dist = Manhattan(c, to);
+          if (buff[it] && !visited[it] && (dist == min_dist)) {
+            visited[it] = true; 
+            open_list.push_back(front + c);
+          }
+        }
+      }
+      delete visited;
+      return false;
     }
 
     const char* test[] = {
@@ -246,6 +302,7 @@ namespace Y18 {
             Attack(buff, w, s, &elves, &goblins, &soldiers, &delete_pool);
 
           } else {
+
             std::vector<vec2i> movements;
             const uint32 count_enemies = (uint32)enemies->size();
             for (uint32 j = 0; j < count_enemies; ++j) {
@@ -269,7 +326,7 @@ namespace Y18 {
               }
             }
 
-            const uint32 count_free_spaces = (uint32)movements.size();
+            uint32 count_free_spaces = (uint32)movements.size();
             if (count_free_spaces > 0) {
               uint32 min_distance = 99999999;
               for (uint32 k = 0; k < count_free_spaces; ++k) {
@@ -291,8 +348,51 @@ namespace Y18 {
                 });
               }
 
-              const vec2i& dest = movements[0];
-              Move(buff, w, s, dest);
+              const vec2i dest = movements[0];
+              movements.clear();
+
+              vec2i n_pos = {0, 0};
+
+              if (FreeSpace(buff, w, s->pos, -1, 0, &n_pos)) {
+                movements.push_back(n_pos);
+              } 
+              
+              if (FreeSpace(buff, w, s->pos, 1, 0, &n_pos)) {
+                movements.push_back(n_pos);
+              } 
+              
+              if (FreeSpace(buff, w, s->pos, 0, -1, &n_pos)) {
+                movements.push_back(n_pos);
+              } 
+              
+              if (FreeSpace(buff, w, s->pos, 0, 1, &n_pos)) {
+                movements.push_back(n_pos);
+              }
+
+              count_free_spaces = (uint32)movements.size();
+              if (count_free_spaces > 0) {
+                min_distance = 99999999;
+                for (uint32 k = 0; k < count_free_spaces; ++k) {
+                  const vec2i& new_pos = movements[k];
+                  const uint32 d = Manhattan(s->pos, new_pos);
+                  if (d < min_distance) {
+                    min_distance = d;
+                  }
+                }
+
+                if (movements.size() > 1) {
+                  movements.erase(std::remove_if(movements.begin(), movements.end(), 
+                    [min_distance, &dest](const vec2i& p) {
+                    return Manhattan(dest, p) > min_distance; 
+                  }));
+
+                  std::sort(movements.begin(), movements.end(), [](const vec2i& l, const vec2i& r) {
+                    return (l.x > r.x) && (l.y < r.y);
+                  });
+                }
+              }
+
+              Move(buff, w, s, movements[0]);
 
               if (CanAttack(buff, w, s, *enemies)) {
                 Attack(buff, w, s, &elves, &goblins, &soldiers, &delete_pool);
---
+++ src/18/16.cpp
@@ -0,0 +1,16 @@
+//jnaranjo Advent of code 18
+
+#include <18/16.h>
+
+#include <utils.h>
+
+namespace Y18 {
+  namespace D16 {
+
+    void puzzle() {
+
+    }
+
+  }
+}
+
